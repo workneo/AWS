e4d56e553181746440e3d5dd3e1087a40da081d6
Navi sureka <navisureka23@gmail.com>
2023-02-21 08:20:25 +0530

    added /abc route on server3

--- server3/server.js
+++ server3/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('server 3 on port 3002');
 });
 
+app.get("/abc",(req,res)=>{
+  res.send("/abc route bae on server3")
+})
+
 app.listen(3002,()=>{
   console.log("Listning on port 3002")
 })
